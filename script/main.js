16a8b6dfbea2cc0c139c48e7e20aa7df79a6fc25
qingzhixing <1418390991@qq.com>
2022-12-14 02:17:51 +0800

    header-text 走马灯

--- script/main.js
+++ script/main.js
@@ -1,3 +1,17 @@
-(function(){
-    
-})()
+window.onload = () => {   
+    let emojisArray = ['🎄', '🐬', '🦌', '🌟', '🌈'];
+
+    let ChangeHeaderText = (emojiId) => {
+        let basicHeaderText = "Merry Christmas";
+        let headerTextElement = document.getElementById('header-text');
+        if (emojiId > emojisArray.length - 1) emojiId = 0;
+        headerTextElement.textContent = basicHeaderText + emojisArray[emojiId];
+
+        setTimeout(function () {
+            ChangeHeaderText(emojiId + 1);
+            // console.log("Recalled ChangeHeaderText()");
+        }, 1000);
+    }
+
+    ChangeHeaderText(0);
+}
