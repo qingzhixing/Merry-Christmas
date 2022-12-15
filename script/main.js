9901eaa36ce6afb84af2bad8a5734f8e9b9a781a
qingzhixing <1418390991@qq.com>
2022-12-15 23:21:32 +0800

    新增加弹球效果对于弹球class每个对象随机化参数设置，增加了弹球img种类

--- script/main.js
+++ script/main.js
@@ -18,6 +18,12 @@ function ChangeHeaderText(emojiId){
 function GenerateMoveDirection(horizontalDirection, verticalDirection) {
     return { horizontalDirection, verticalDirection };
 }
+function GenerateRandomMoveDirection(){
+    let horizontalDirection=(Math.random()>=0.5)? 1:-1;
+    let verticalDirection=(Math.random()>=0.5)? 1:-1;
+    return GenerateMoveDirection(horizontalDirection, verticalDirection);
+}
+
 function GeneratePosition(x, y) {
     return { x, y };
 }
@@ -29,55 +35,66 @@ function GetElementPositionInfo(element) {
         height: element.offsetHeight
     };
 }
+function GenerateMarble(element) {
+    const maxSpeed = 10;
+    function StartMarble() {
+        // console.log('StartMarble');
+        let element=this.element;
+        let elementPositionInfo = GetElementPositionInfo(element);
+        let windowWidth = window.innerWidth;
+        let windowHeight = window.innerHeight;
+        let moveDirection=this.moveDirection;
+        let moveSpeed = this.moveSpeed;
 
-function StartMarble(element, moveDirection) {
-    // console.log('StartMarble');
-    let elementPositionInfo = GetElementPositionInfo(element);
-    let windowWidth = window.innerWidth;
-    let windowHeight = window.innerHeight;
+        let nextPosition = GeneratePosition(elementPositionInfo.leftPosition, elementPositionInfo.topPosition);
+        //horizontal
+        if (1 == moveDirection.horizontalDirection) {
+            nextPosition.x += moveSpeed;
+            if (nextPosition.x+elementPositionInfo.width >= windowWidth) {
+                nextPosition.x = windowWidth - elementPositionInfo.width;
+                moveDirection.horizontalDirection = -1;
+            }
+        } else {
+            nextPosition.x -= moveSpeed;
+            if (nextPosition.x <= 0) {
+                nextPosition.x = 0;
+                moveDirection.horizontalDirection = 1;
+            }
+        }
+        //vertical
+        if (1 == moveDirection.verticalDirection) {
+            nextPosition.y += moveSpeed;
+            if (nextPosition.y+elementPositionInfo.height >= windowHeight) {
+                nextPosition.y = windowHeight - elementPositionInfo.height;
+                moveDirection.verticalDirection = -1;
+            }
+        } else {
+            nextPosition.y -= moveSpeed;
+            if (nextPosition.y <= 0) {
+                nextPosition.y = 0;
+                moveDirection.verticalDirection = 1;
+            }
+        }
 
-    const moveSpeed = 3;
-    let nextPosition = GeneratePosition(elementPositionInfo.leftPosition, elementPositionInfo.topPosition);
-    //horizontal
-    if (1 == moveDirection.horizontalDirection) {
-        nextPosition.x += moveSpeed;
-        if (nextPosition.x+elementPositionInfo.width >= windowWidth) {
-            nextPosition.x = windowWidth - elementPositionInfo.width;
-            moveDirection.horizontalDirection = -1;
-        }
-    } else {
-        nextPosition.x -= moveSpeed;
-        if (nextPosition.x <= 0) {
-            nextPosition.x = 0;
-            moveDirection.horizontalDirection = 1;
-        }
+        element.style.left = nextPosition.x + 'px';
+        element.style.top = nextPosition.y + 'px';
+        setTimeout(() => { this.StartMarble() }, 18);
+
     }
-    //vertical
-    if (1 == moveDirection.verticalDirection) {
-        nextPosition.y += moveSpeed;
-        if (nextPosition.y+elementPositionInfo.height >= windowHeight) {
-            nextPosition.y = windowHeight - elementPositionInfo.height;
-            moveDirection.verticalDirection = -1;
-        }
-    } else {
-        nextPosition.y -= moveSpeed;
-        if (nextPosition.y <= 0) {
-            nextPosition.y = 0;
-            moveDirection.verticalDirection = 1;
-        }
+    return {
+        moveSpeed: Math.random() * maxSpeed,
+        moveDirection: GenerateRandomMoveDirection(),
+        element: element,
+        StartMarble: StartMarble
     }
-
-    element.style.left = nextPosition.x + 'px';
-    element.style.top = nextPosition.y + 'px';
-    setTimeout(() => { StartMarble(element, moveDirection) }, 18);
-
 }
+
 
 function MoveMarble() {
     let marbleElements = document.getElementsByClassName('marble');
     for (let i = 0; i < marbleElements.length; i++) {
         let marble = marbleElements[i];
-        StartMarble(marble,GenerateMoveDirection(1,1))
+        GenerateMarble(marble).StartMarble();
     }
 }
 
